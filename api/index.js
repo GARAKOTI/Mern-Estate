6b98b14c3db5c40e189bb73a087b957bc54d767c
GARAKOTI <rahulsinghgarakoti730@gamil.com>
2024-01-27 17:42:53 +0530

    created middleware and coustom error handlers

--- api/index.js
+++ api/index.js
@@ -1,23 +1,38 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("conneted to data base sucessfully")
-}).catch((err)=>{
-    console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("conneted to data base sucessfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/user",userRouter)
-app.use("/api/auth",authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-app.listen(3000, () =>{
-    console.log("server is running on port 3000")
+//creating a middleware
+
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
 })
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
